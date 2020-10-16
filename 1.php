a48bf5264c9e7c2c95c31a43a965021c46a804b7
ilhamramadani <ilham@aleph-labs.com>
2020-10-16 21:34:46 +0700

    change logit if to switch case

--- 1.php
+++ 1.php
@@ -1,36 +1,38 @@
 <?php
 function hitungBarang($type, $qty) {
-    if ($type == "A") {
-        $harga = 3000;
-        
-        if ($qty > 10) {
-            $bonus = $harga - 500;
-            $total = $bonus * $qty;
+    switch ($type) {
+        case "A":
+            $harga = 3000;
+            if ($qty > 10) {
+                $bonus = $harga - 500;
+                $total = $bonus * $qty;
+                
+                echo 'Total Harga Barang = Rp. \n', $qty * $harga ;
+                echo 'Potongan Harga Barang = Rp. \n', $qty * 5 ;
+                echo 'Total Harga yang dibayar = Rp. ', $total;
+            }
+            break;
+        case "B":
+            $harga = 3500;
+            if ($qty > 5) {
+                $bonus = $harga * $qty * 50 / 100;
+                $total = ($harga * $qty) - $bonus;
+                
+                echo 'Total Harga Barang = Rp. \n', $qty * $harga;
+                echo 'Potongan Harga Barang = Rp. \n', $bonus;
+                echo 'Total Harga yang dibayar = Rp. \n', $total;
+            }
+            break;
+        default:
+            $harga = 5000;
+            $total = $harga * $qty;
             
-            echo 'Total Harga Barang = Rp. '+ $qty * $harga ;
-            echo 'Potongan Harga Barang = Rp. '+ $qty * 5 ;
-            echo 'Total Harga yang dibayar = Rp. '+ $total;
-        }
+            echo 'Total Harga Barang = Rp. \n', $total;
+            echo 'Potongan Harga Barang = Rp. 0 \n';
+            echo 'Total Harga yang dibayar = Rp. \n', $total;
     }
-    else if ($type == "A"){
-        $harga = 3500;
-        if ($qty > 5) {
-            $bonus = $harga * $qty * 50 / 100;
-            $total = ($harga * $qty) - $bonus;
-            
-            echo 'Total Harga Barang = Rp. '+($qty * $harga);
-            echo 'Potongan Harga Barang = Rp. '+ $bonus;
-            echo 'Total Harga yang dibayar = Rp. '+ $total;
-        }
-    }
-    else {
-        $harga = 5000;
-        $total = $harga * $qty;
-        
-        echo 'Total Harga Barang = Rp. '+ $total;
-        echo 'Potongan Harga Barang = Rp. 0';
-        echo 'Total Harga yang dibayar = Rp. '+ $total;
-    }
+
+    
 }
 
 hitungBarang("A", 11);
